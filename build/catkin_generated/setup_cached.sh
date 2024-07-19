62939b51141d66fcd61dd681a15df484507a59f7
chp <re23m025@technikum-wien.at>
2024-07-19 15:18:21 +0200

    RobAssemble und RobShift werden beide in gazebo geladen und können grundsätzlich gesteuert werden

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH='/opt/ros/noetic/lib'
-export PKG_CONFIG_PATH='/opt/ros/noetic/lib/pkgconfig'
 export PWD='/home/chp/ROD/build'
-export ROS_PACKAGE_PATH='/home/chp/ROD/src:/opt/ros/noetic/share'
