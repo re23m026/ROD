7b1229f47e97873a507caa3bf0cf16f021162636
re23m026 <re23m026@technikum-wien.at>
2024-09-15 20:17:59 +0200

    move script for rob assem works for many points

--- src/project_assem/src/move_robot.py
+++ src/project_assem/src/move_robot.py
@@ -21,6 +21,41 @@ def move_robot():
     move_group = moveit_commander.MoveGroupCommander(group_name)
     # move_group.set_planning_time(10)  # Setze die Planungszeit auf 10 Sekunden
 
+
+
+
+    ## Create a `DisplayTrajectory`_ ROS publisher which is used to display
+    ## trajectories in Rviz:
+    display_trajectory_publisher = rospy.Publisher(
+        "/move_group/display_planned_path",
+        moveit_msgs.msg.DisplayTrajectory,
+        queue_size=20,
+    )
+
+
+    # We can get the name of the reference frame for this robot:
+    planning_frame = move_group.get_planning_frame()
+    print("============ Planning frame: %s" % planning_frame)
+
+    # We can also print the name of the end-effector link for this group:
+    eef_link = move_group.get_end_effector_link()
+    print("============ End effector link: %s" % eef_link)
+
+
+    ## TODO
+
+    #   def go_to_joint_state(self):
+    #   def go_to_pose_goal(self):
+    #   def plan_cartesian_path(self):
+    #   def display_trajectory(self, plan):
+    #   def execute_plan(self, plan):
+    #   def wait_for_state_update(self, box known, box attached, timeout)
+    #   def add_box(self, timeout)
+    #   def attach_box(self)
+    #   def detach_box(self)
+
+
+
     # Zielpose definieren
     pose_target = geometry_msgs.msg.Pose()
     pose_target.orientation.w = 0.0
@@ -31,6 +66,22 @@ def move_robot():
 
     # Bewegung planen und ausführen
     plan = move_group.go(wait=True)
+
+    # Zielpose definieren
+    pose_target = geometry_msgs.msg.Pose()
+    pose_target.orientation.w = 0.0
+    pose_target.position.x = 5.0
+    pose_target.position.y = 4.0
+    pose_target.position.z = 2.0
+    move_group.set_pose_target(pose_target)
+
+    # Bewegung planen und ausführen
+    plan = move_group.go(pose_target, wait=True)
+
+
+
+
+
 
     # Ziele löschen
     move_group.stop()
