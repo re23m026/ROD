8f19a3d83be453186e887ee9a07d65bc037ba3da
re23m026 <re23m026@technikum-wien.at>
2024-09-17 11:47:15 +0200

    function for adding boxes

--- src/project_assem/src/move_test.py
+++ src/project_assem/src/move_test.py
@@ -19,6 +19,7 @@ def move_robot():
     move_group = moveit_commander.MoveGroupCommander(group_name)
     move_group.set_max_velocity_scaling_factor(1.0)  # Setzen Sie 1.0 für maximale Geschwindigkeit (100%)
     move_group.set_max_acceleration_scaling_factor(1.0)  # Setzen Sie 1.0 für maximale Beschleunigung (100%)
+    
 
     # Create a DisplayTrajectory ROS publisher for Rviz
     display_trajectory_publisher = rospy.Publisher(
@@ -26,6 +27,15 @@ def move_robot():
         moveit_msgs.msg.DisplayTrajectory,
         queue_size=20,
     )
+
+    # Box hinzufügen
+    add_object(scene, 2.0, 2.2, 0.3, "box_1")
+    add_object(scene, 2.0, 2.4, 0.3, "box_2")
+    add_object(scene, 2.0, 2.6, 0.3, "box_3")
+    add_object(scene, 3.0, 2.2, 0.3, "box_4")
+    add_object(scene, 3.0, 2.4, 0.3, "box_5")
+    add_object(scene, 2.0, 2.6, 0.3, "box_6")
+
 
     # Zielpose für "Pick" definieren
     pick_pose = geometry_msgs.msg.Pose()
@@ -49,9 +59,9 @@ def move_robot():
     # Zielpose für "Place" definieren
     place_pose = geometry_msgs.msg.Pose()
     place_pose.orientation.w = 1.0
-    place_pose.position.x = 3.0
-    place_pose.position.y = 1.2
-    place_pose.position.z = 2.0
+    place_pose.position.x = 0.0
+    place_pose.position.y = 0.2
+    place_pose.position.z = 1.0
     move_group.set_pose_target(place_pose)
     move_group.set_planning_time(10)  # Setze die Planungszeit auf 10 Sekunden
 
@@ -68,7 +78,7 @@ def move_robot():
     # Herunterfahren
     moveit_commander.roscpp_shutdown()
 
-def add_object(scene):
+def grasp_object(robot, move_group, scene):
     # Fügen Sie hier den Code hinzu, um das Objekt an das Werkzeug zu "attachen"
     # Zum Beispiel eine Box als Platzhalter für das Objekt hinzufügen und anhängen
     box_name = "object"
@@ -76,7 +86,7 @@ def add_object(scene):
     box_pose.header.frame_id = move_group.get_end_effector_link()
     box_pose.pose.orientation.w = 1.0
     box_pose.pose.position.z = 0.1  # Höhe relativ zum Endeffektor
-    scene.add_box(box_name, box_pose, size=(0.05, 0.05, 0.05))
+    scene.add_box(box_name, box_pose, size=(0.2, 0.2, 0.2))
 
     # Warten, bis die Box in der Szene hinzugefügt wurde
     rospy.sleep(1)
@@ -87,33 +97,25 @@ def add_object(scene):
     scene.attach_box(eef_link, box_name, touch_links=touch_links)
 
 
-def grasp_object(robot, move_group, scene):
+def add_object(scene, x, y, z, name):
     # Definiere den Namen des Objekts
-    box_name = "object"
+    box_name = name
 
     # Position des Objekts in der Welt (angepasst an die tatsächliche Position in Ihrer Simulation)
     box_pose = geometry_msgs.msg.PoseStamped()
     box_pose.header.frame_id = "world"  # Oder ein anderer Referenzrahmen, z.B. das Förderband
     box_pose.pose.orientation.w = 1.0
-    box_pose.pose.position.x = 2.2  # Setzen Sie die tatsächlichen Koordinaten des Objekts
-    box_pose.pose.position.y = 2.1
-    box_pose.pose.position.z = 0.5  # Höhe über dem Boden oder dem Förderband
+    box_pose.pose.position.x = x  # Setzen Sie die tatsächlichen Koordinaten des Objekts
+    box_pose.pose.position.y = y
+    box_pose.pose.position.z = z  # Höhe über dem Boden oder dem Förderband
 
     # Objekt zur Szene hinzufügen
-    scene.add_box(box_name, box_pose, size=(0.05, 0.05, 0.05))
+    scene.add_box(box_name, box_pose, size=(0.1, 0.1, 0.1))
 
     # Warten, bis das Objekt in der Szene hinzugefügt wurde
     rospy.sleep(1)
 
-    # # Objekt greifen: Anhängen des Objekts an den Endeffektor
-    eef_link = move_group.get_end_effector_link()
-    touch_links = robot.get_link_names(group=move_group.get_name())
-
-    # # Anhängen des Objekts
-    scene.attach_box(eef_link, box_name, touch_links=touch_links)
-
     # # Warten, bis das Objekt angehängt wurde
-    rospy.sleep(1)
 
 
 def release_object(move_group, scene):
